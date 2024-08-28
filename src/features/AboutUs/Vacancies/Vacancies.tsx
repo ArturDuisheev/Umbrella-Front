66615a718a710bd4bc062027d5203a3400cb65dc
KatanaTakahashi <KatsuroTakahashi05@yandex.ru>
2024-08-28 11:17:08 +0600

    first commit

---
+++ src/features/AboutUs/Vacancies/Vacancies.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import { selectVacancies } from '@/features/AboutUs/Vacancies/vacanciesSlice';
+import { fetchVacancies } from '@/features/AboutUs/Vacancies/vacanciesThunk';
+import VacancyCard from '@/features/AboutUs/Vacancies/components/VacancyCard/VacancyCard';
+import './Vacancies.scss';
+
+const Vacancies = () => {
+  const dispatch = useAppDispatch();
+  const vacancies = useAppSelector(selectVacancies);
+
+  useEffect(() => {
+    dispatch(fetchVacancies());
+  }, [dispatch]);
+
+  if (vacancies.length === 0) {
+    return <div style={{ height: '230px' }} />;
+  }
+
+  return (
+    <section className="vacancies container">
+      <h3 className="vacancies-title">Вакансии</h3>
+      <div className="vacancies-cards">
+        {vacancies.map((vacancy) => (
+          <VacancyCard key={vacancy.id} vacancy={vacancy} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Vacancies;
